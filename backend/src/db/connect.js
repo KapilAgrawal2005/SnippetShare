104b631fa7cc9a7c048cf86b09c5ffa7285b17a8
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-06-13 11:06:53 +0530

    updated the batabase connection

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
-    await mongoose.connect(process.env.DATABASE_URL, {});
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to database.....");
   } catch (error) {
     console.log("Failed to connect to database.....", error.message);
