7d269058df7c7ec547af5c3cddfb05b1748a3ae4
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-06-22 16:50:52 +0530

    Create all functions for the snippet apis

--- frontend/src/context/snippetContext.js
+++ frontend/src/context/snippetContext.js
@@ -1,4 +1,3 @@
-"use client";
 import axios from "axios";
 import React, {
   createContext,
@@ -7,13 +6,53 @@ import React, {
   useMemo,
   useState,
 } from "react";
+import toast from "react-hot-toast";
+import { useGlobalContext } from "./globalContext";
+import { useUserContext } from "./userContext";
 
-const SnippetContext = createContext();
+const SnippetsContext = createContext();
 
-export const SnippetProvider = ({ children }) => {
+export const SnippetsProvider = ({ children }) => {
+  const { closeModal } = useGlobalContext();
   const serverUrl = "http://localhost:4000/api/v1";
 
+  const userId = useUserContext().user?._id;
+
   const [publicSnippets, setPublicSnippets] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [userSnippets, setUserSnippets] = useState([]);
+  const [likedSnippets, setLikedSnippets] = useState([]);
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [popularSnippets, setPopularSnippets] = useState([]);
+
+  const createSnippet = async (data) => {
+    try {
+      const res = await axios.post(`${serverUrl}/create-snippet`, data);
+
+      setPublicSnippets([res.data, ...publicSnippets]);
+
+      getPublicSnippets();
+
+      toast.success("Snippet created successfully");
+
+      closeModal();
+    } catch (error) {
+      console.log("Error creating snippet", error);
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const updateSnippet = async (data) => {
+    try {
+      await axios.patch(`${serverUrl}/snippet/${data._id}`, data);
+
+      getPublicSnippets();
+      toast.success("Snippet updated successfully");
+    } catch (error) {
+      console.log("Error updating snippet", error);
+    }
+  };
 
   const getPublicSnippets = async (userId, tagId, searchQuery, page) => {
     try {
@@ -49,6 +88,148 @@ export const SnippetProvider = ({ children }) => {
     } catch (error) {
       console.log("Error fetching public snippets", error);
       return [];
+    }
+  };
+
+  const getUserSnippets = async (tagId, search) => {
+    setLoading(true);
+    try {
+      const queryParams = new URLSearchParams();
+
+      if (tagId) {
+        queryParams.append("tagId", tagId);
+      }
+
+      if (search) {
+        queryParams.append("search", search);
+      }
+
+      const res = await axios.get(
+        `${serverUrl}/snippets?${queryParams.toString()}`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      setLoading(false);
+
+      setUserSnippets(res.data);
+
+      return res.data;
+    } catch (error) {
+      console.log("Error fetching user snippets", error);
+    }
+  };
+
+  const getLikedSnippets = async (tagId, search) => {
+    setLoading(true);
+    try {
+      const queryParams = new URLSearchParams();
+
+      if (tagId) {
+        queryParams.append("tagId", tagId);
+      }
+
+      if (search) {
+        queryParams.append("search", search);
+      }
+
+      const res = await axios.get(
+        `${serverUrl}/snippets/liked?${queryParams.toString()}`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      setLoading(false);
+      setLikedSnippets(res.data);
+      return res.data;
+    } catch (error) {
+      console.log("Error fetching liked snippets", error);
+    }
+  };
+
+  const getPublicSnippetById = async (id) => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${serverUrl}/snippet/public/${id}`);
+
+      setLoading(false);
+      return res.data;
+    } catch (error) {
+      console.log("Error fetching snippet by id", error);
+    }
+  };
+
+  const getTags = async () => {
+    try {
+      const res = await axios.get(`${serverUrl}/tags`);
+      setTags(res.data.tags);
+    } catch (error) {
+      console.log("Error fetching tags", error);
+    }
+  };
+
+  const likeSnippet = async (id) => {
+    try {
+      await axios.patch(`${serverUrl}/snippet/like/${id}`);
+    } catch (error) {
+      console.log("Error liking snippet", error);
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const deleteSnippet = async (id) => {
+    try {
+      await axios.delete(`${serverUrl}/snippet/${id}`);
+      toast.success("Snippet deleted successfully");
+      getPublicSnippets();
+    } catch (error) {
+      console.log("Error deleting snippet", error);
+      toast.error(error.response.data.message);
+    }
+  };
+
+  // get leaderboard
+  const getLeaderboard = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${serverUrl}/leaderboard`);
+
+      setLoading(false);
+      setLeaderboard(res.data);
+
+      return res.data;
+    } catch (error) {
+      console.log("Error fetching leaderboard", error);
+    }
+  };
+
+  // popular snippets
+  const getPopularSnippets = async (tagId, search) => {
+    setLoading(true);
+    try {
+      const queryParams = new URLSearchParams();
+
+      if (tagId) {
+        queryParams.append("tagId", tagId);
+      }
+
+      if (search) {
+        queryParams.append("search", search);
+      }
+
+      const res = await axios.get(
+        `${serverUrl}/snippets/popular?${queryParams.toString()}`
+      );
+
+      setLoading(false);
+
+      setPopularSnippets(res.data);
+
+      return res.data;
+    } catch (error) {
+      console.log("Error fetching popular snippets", error);
     }
   };
 
@@ -93,29 +274,56 @@ export const SnippetProvider = ({ children }) => {
       Math.floor(Math.random() * Object.values(gradients).length)
     ];
 
+  // memo
   const useBtnColorMemo = useMemo(() => randomButtonColor, []);
   const useTagColorMemo = useMemo(() => randomTagColor, []);
 
+  // Log when tags state actually updates
+  useEffect(() => {
+    console.log("Tags state updated:", tags);
+  }, [tags]);
+
   useEffect(() => {
     getPublicSnippets();
-    // getTags();
-    // getLeaderboard();
-    // getPopularSnippets();
+    getTags();
+    getLeaderboard();
+    getPopularSnippets();
   }, []);
 
+  console.log("Leaderboard", leaderboard);
+
+  console.log("Popular Snippets", popularSnippets);
+
   return (
-    <SnippetContext.Provider
+    <SnippetsContext.Provider
       value={{
         publicSnippets,
         getPublicSnippets,
         useBtnColorMemo,
         useTagColorMemo,
+        createSnippet,
+        tags,
+        updateSnippet,
+        deleteSnippet,
+        likeSnippet,
+        getPublicSnippetById,
+        loading,
+        getUserSnippets,
+        userSnippets,
+        getLikedSnippets,
+        likedSnippets,
+        getLeaderboard,
+        leaderboard,
+        getPopularSnippets,
+        popularSnippets,
+        getTags,
       }}
     >
       {children}
-    </SnippetContext.Provider>
+    </SnippetsContext.Provider>
   );
 };
-export function useSnippetContext() {
-  return useContext(SnippetContext);
-}
+
+export const useSnippetContext = () => {
+  return useContext(SnippetsContext);
+};
