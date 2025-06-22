2391e6078210700ba0a9f4dce7561a937f8f2819
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-06-22 16:56:41 +0530

    Added the button for creating snippet and search and also for profile

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -7,8 +7,13 @@ import logo from "@/../public/logo.png";
 import SearchInput from "../searchInput/searchInput";
 import { login, register } from "@/utils/Icons";
 import { useRouter } from "next/navigation";
+import { useGlobalContext } from "@/context/globalContext";
+import SearchIcon from "../../../public/Icons/SearchIcon";
 const Header = () => {
   const { user } = useUserContext();
+
+  const { openModalForSnippet, openProfileModal, openModalForSearch } =
+    useGlobalContext();
 
   const photo = user?.photo;
 
@@ -32,24 +37,55 @@ const Header = () => {
         <SearchInput />
       </div>
 
-      <div className="flex items-center gap-4">
-        <button
-          className="btn-hover relative h-[47px] px-8 bg-[#3A3B3C] flex items-center justify-center gap-4 rounded-xl overflow-hidden hover:cursor-pointer"
-          onClick={() => router.push("/login")}
-        >
-          <span className="text-xl text-gray-200">{login}</span>
-          <span className="font-bold text-white">Login</span>
-          <div className="blob"></div>
-        </button>
-        <button
-          className="btn-hover relative h-[47px] px-8 bg-[#7263F3] flex items-center justify-center gap-4 rounded-xl overflow-hidden hover:cursor-pointer"
-          onClick={() => router.push("/register")}
-        >
-          <span className="text-xl text-gray-200">{register}</span>
-          <span className="font-bold text-white">Register</span>
-          <div className="blob"></div>
-        </button>
-      </div>
+      {!user._id ? (
+        <div className="flex items-center gap-4">
+          <button
+            className="btn-hover relative h-[47px] px-8 bg-[#3A3B3C] flex items-center justify-center gap-4 rounded-xl overflow-hidden hover:cursor-pointer"
+            onClick={() => router.push("/login")}
+          >
+            <span className="text-xl text-gray-200">{login}</span>
+            <span className="font-bold text-white">Login</span>
+            <div className="blob"></div>
+          </button>
+          <button
+            className="btn-hover relative h-[47px] px-8 bg-[#7263F3] flex items-center justify-center gap-4 rounded-xl overflow-hidden hover:cursor-pointer"
+            onClick={() => router.push("/register")}
+          >
+            <span className="text-xl text-gray-200">{register}</span>
+            <span className="font-bold text-white">Register</span>
+            <div className="blob"></div>
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center gap-2">
+          <button
+            className="mr-4 h-[42px] px-4 flex items-center justify-center bg-white rounded-lg font-semibold hover:bg-white/80 transition duration-200 ease-in-out"
+            onClick={openModalForSnippet}
+          >
+            Create Snippet
+          </button>
+
+          <button
+            onClick={openModalForSearch}
+            className="w-[45px] h-[45px] flex items-center justify-center bg-[#ffffff0d] rounded-lg lg:hidden"
+          >
+            <SearchIcon stroke="rgba(249,249,249,0.6) "></SearchIcon>
+          </button>
+
+          <button
+            onClick={openProfileModal}
+            className="w-[43px] h-[42px] flex justify-center items-center bg-[#ffffff0d] rounded-lg"
+          >
+            <Image
+              src={photo || "/images/User.png"}
+              alt="profile"
+              width={35}
+              height={35}
+              className="rounded-lg"
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
